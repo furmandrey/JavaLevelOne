fb84243749be48ad113957c424eb0d59c0946251
AndrieiFurman <>
2021-12-22 07:17:33 +0000

    HW_5 + setter.

--- HomeWork5.java
+++ HomeWork5.java
@@ -6,7 +6,7 @@
  */
 //ООП
 
-import java.util.* ;
+import java.util. * ;
 
 public class HomeWork5 {
     public static void main(String[]args) {
@@ -14,20 +14,21 @@ public class HomeWork5 {
         //System.out.println(employee.display());
         System.out.println(employee);
 
-        Employee[] employeeArray = new Employee[5];
+        Employee[]employeeArray = new Employee[5];
         employeeArray[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@gmail.com", "+7(916)2223344", 10000, 50);
-        employeeArray[1] = new Employee("Peter Hubbard","Engineer", "Peter@mgmail.com", "+7(915)1113344", 10000, 35);
-        employeeArray[2] = new Employee("Dwayne Buttler","manager", "Dwayne@gmail.com", "+7(918)5553344", 10000, 39);
-        employeeArray[3] = new Employee("Clifton Walsh","manager", "Clifton@gmail.com", "+7(916)2221100", 10000, 41);
-        employeeArray[4] = new Employee("Anthony Good","manager", "Anthony@gmail.com", "+7(916)2226677", 10000, 45);
+        employeeArray[1] = new Employee("Peter Hubbard", "Engineer", "Peter@mgmail.com", "+7(915)1113344", 10000, 35);
+        employeeArray[2] = new Employee("Dwayne Buttler", "manager", "Dwayne@gmail.com", "+7(918)5553344", 10000, 39);
+        employeeArray[3] = new Employee("Clifton Walsh", "manager", "Clifton@gmail.com", "+7(916)2221100", 10000, 41);
+        employeeArray[4] = new Employee("Anthony Good", "manager", "Anthony@gmail.com", "+7(916)2226677", 10000, 45);
 
         employeeArray[4].setSalary(11000);
 
-            for (int i = 0; i < employeeArray.length; i++) {
-                 if (employeeArray[i].getAge() > 40) System.out.println(employeeArray[i].display());
-                 //if (employeeArray[i].getAge() > 40) employeeArray[i].show();
-            }
-            
+        for (int i = 0; i < employeeArray.length; i++) {
+            if (employeeArray[i].getAge() > 40)
+                System.out.println(employeeArray[i].display());
+            //if (employeeArray[i].getAge() > 40) employeeArray[i].show();
+        }
+
     }
 }
 class Employee {
@@ -51,11 +52,10 @@ class Employee {
     }
 
     public void setSalary(int salary) {
-      if (salary != 0) {
-        this.salary = salary;
-      }
+        if (salary != 0) {
+            this.salary = salary;
+        }
     }
-
 
     String display() {
         return fullName + ", " + position + ", " + email + ", " + phoneNumber + ", " + salary + ", " + age;
@@ -71,6 +71,6 @@ class Employee {
 
     public String toString() {
         return fullName + ", " + position + ", " + email + ", " + phoneNumber + ", " + salary + ", " + age;
-        
+
     }
 }
